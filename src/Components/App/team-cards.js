586d6e6a3c6ec0700d850a2bf32caa29d9445ce4
Chia <aesparzachavez@dons.usfca.edu>
2020-10-26 12:18:57 -0700

    adding Tim & Stokala's cards

--- src/Components/App/team-cards.js
+++ src/Components/App/team-cards.js
@@ -1,13 +1,15 @@
+import tim from "./tim.jpg"
 import chia from "./chia.jpeg"
 import aditya from "./aditya.jpeg"
 import jochem from "./jochem.jpeg"
+import stokala from "./stokala.png"
 
 export const cardData = [
 
  {
   title: "Tim Castillo",
   excerpt: "Our Sponsor!",
-  image: "https://bit.ly/2WNi2Ml"
+  image: tim
  },
 
  {
@@ -26,5 +28,11 @@ export const cardData = [
   title: "Jochem",
   excerpt: "Student at USF",
   image: jochem
+ },
+
+ {
+  title: "Stokala",
+  excerpt: "Student at USF",
+  image: stokala
  },
 ]
